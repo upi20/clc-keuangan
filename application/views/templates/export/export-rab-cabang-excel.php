924683d80d5ec5fb7815ad4cd2b467e00a1d6161
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-04-14 14:26:09 +0700

    Update export

--- application/views/templates/export/export-rab-cabang-excel.php
+++ application/views/templates/export/export-rab-cabang-excel.php
@@ -5,6 +5,14 @@
 <head>
     <title>Export Data</title>
     <style>
+        .str {
+            mso-number-format: \@;
+        }
+
+        .number {
+            mso-number-format: \#;
+        }
+
         #table {
             font-family: Arial, Helvetica, sans-serif;
             border-collapse: collapse;
@@ -74,24 +82,24 @@
                 <tbody>
                     <?php foreach ($data as $d) : ?>
                         <tr>
-                            <td><?= $d['nama_cabang']; ?></td>
-                            <td><?= $d['uraian']; ?></td>
-                            <td><?= $d['kodes']; ?></td>
-                            <td><?= $d['nama_aktifitas']; ?></td>
-                            <td><?= $d['jumlah_1']; ?></td>
-                            <td><?= $d['satuan_1']; ?></td>
-                            <td><?= $d['jumlah_2']; ?></td>
-                            <td><?= $d['satuan_2']; ?></td>
-                            <td><?= $d['jumlah_3']; ?></td>
-                            <td><?= $d['satuan_3']; ?></td>
-                            <td><?= $d['jumlah_4']; ?></td>
-                            <td><?= $d['satuan_4']; ?></td>
-                            <td><?= number_format($d['harga_ringgit'], 0, ".", "."); ?></td>
-                            <td><?= number_format($d['harga_rupiah'], 0, ",", ","); ?></td>
-                            <td><?= number_format($d['total_harga_ringgit'], 0, ".", "."); ?></td>
-                            <td><?= number_format($d['total_harga_rupiah'], 0, ",", ","); ?></td>
-                            <td><?= $d['prioritas']; ?></td>
-                            <td><?= $d['statuss']; ?></td>
+                            <td class="str"><?= $d['nama_cabang']; ?></td>
+                            <td class="str"><?= $d['uraian']; ?></td>
+                            <td class="str"><?= $d['kodes']; ?></td>
+                            <td class="str"><?= $d['nama_aktifitas']; ?></td>
+                            <td class="number"><?= $d['jumlah_1']; ?></td>
+                            <td class="str"><?= $d['satuan_1']; ?></td>
+                            <td class="number"><?= $d['jumlah_2']; ?></td>
+                            <td class="str"><?= $d['satuan_2']; ?></td>
+                            <td class="number"><?= $d['jumlah_3']; ?></td>
+                            <td class="str"><?= $d['satuan_3']; ?></td>
+                            <td class="number"><?= $d['jumlah_4']; ?></td>
+                            <td class="str"><?= $d['satuan_4']; ?></td>
+                            <td class="number"><?= $d['harga_ringgit']; ?></td>
+                            <td class="number"><?= $d['harga_rupiah']; ?></td>
+                            <td class="number"><?= $d['total_harga_ringgit']; ?></td>
+                            <td class="number"><?= $d['total_harga_rupiah']; ?></td>
+                            <td class="str"><?= $d['prioritas']; ?></td>
+                            <td class="str"><?= $d['statuss']; ?></td>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
